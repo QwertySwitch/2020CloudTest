66e8a10a24060566c64e846f6a2fe5a1bbac4412
QwertySwitch <artfjqm2@naver.com>
2020-06-03 14:52:37 +0900

    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int[] no;
+        static string[] stype;
+        static string[] sname;
+        static string[] addr1;
+        static string[] addr2;
+        static string[] lat;
+        static string[] longt;
+        static string[] iscctv;
+        static int[] cctvcount;
+        static DataSet ds = new DataSet();
+        static string connStr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\artfj\\Desktop\\통합 문서1.accdb;MODE=ReadWrite;";
+        
+        private static void count_info1()
+        {
+            string sql = "Select * FROM Sheet1";
+            OleDbConnection conn = new OleDbConnection(connStr);
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
+
+            cmd.CommandText = sql;
+            int count = 0;
+            conn.Open();
+            OleDbDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                count++;
+            }
+            stype = new string[count];
+            sname = new string[count];
+            addr1 = new string[count];
+            addr2 = new string[count];
+            lat = new string[count];
+            longt = new string[count];
+            iscctv = new string[count];
+            cctvcount = new int[count];
+
+            Console.WriteLine("완료 {0}", count);
+            reader.Close();
+            conn.Close();
+        }
+        private static void search_info()
+        {
+            string sql = "Select * FROM Sheet1";
+            OleDbConnection conn = new OleDbConnection(connStr);
+            OleDbCommand cmd = new OleDbCommand(sql, conn);
+
+            cmd.CommandText = sql;
+            int i = 0;
+
+            conn.Open();
+            OleDbDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                stype[i] = reader["시설종류"].ToString();
+                sname[i] = reader["대상시설명"].ToString();
+                addr1[i] = reader["소재지도로"].ToString();
+                addr2[i] = reader["소재지지번"].ToString();
+                lat[i] = reader["위도"].ToString();
+                longt[i] = reader["경도"].ToString();
+                iscctv[i] = reader["CCTV설치유무"].ToString();
+                cctvcount[1] = int.Parse(reader["CCTV설치대수"].ToString());
+                i++;
+            }
+            reader.Close();
+            conn.Close();
+            Console.WriteLine("완료 {0}", i);
+        }
+        static void Main(string[] args)
+        {
+            OleDbConnection conn = new OleDbConnection(connStr);
+            string sql1 = "SELECT * FROM Sheet1";
+            OleDbDataAdapter adp = new OleDbDataAdapter(sql1, conn);
+            adp.Fill(ds);
+            count_info1();
+            search_info();
+
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "silladbserver.database.windows.net";
+                builder.UserID = "switch";
+                builder.Password = "kookang1234;;";
+                builder.InitialCatalog = "clouddbtest";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data ADD:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    SqlCommand command = null;
+                    for (int i = 0; i<stype.Length; i++)
+                    {
+                        string sql = "INSERT INTO SECURITY VALUES ('" + stype[i] + "','" + sname[i] + "','" + addr1[i] + "','" + addr2[i] + "','"+lat+"','" + longt + "','" + iscctv + "',"+cctvcount+");";
+                        command = new SqlCommand(sql, connection);
+                        command.ExecuteNonQuery();
+                    }
+                    
+                    StringBuilder sb1 = new StringBuilder();
+                    sb1.Append("SELECT ADDR FROM BELL");
+                    
+                    String sql2 = Convert.ToString(sb1.ToString());
+                    
+                    using (SqlCommand command2 = new SqlCommand(sql2, connection))
+                    {
+                        using (SqlDataReader reader = command2.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0}", reader.GetString(0));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.IO;
+using System.Web;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static string[] addr1 = new string[10];
+        public static string[] sname;
+        public static string[] stype;
+        public static string[] lat;
+        public static string[] longt;
+        public static string[] iscctv;
+        public static int[] cctvcount;
+
+
+        
+        public static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "silladbserver.database.windows.net";
+                builder.UserID = "switch";
+                builder.Password = "kookang1234;;";
+                builder.InitialCatalog = "clouddbtest";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    int x = 0;
+                    connection.Open();                    
+
+                    StringBuilder sb1 = new StringBuilder();
+                    sb1.Append("SELECT schooltype,schoolname,addr1,lat,long,iscctv,cctvcount FROM SECURITY");
+
+                    String sql2 = Convert.ToString(sb1.ToString());
+
+                    using (SqlCommand command2 = new SqlCommand(sql2, connection))
+                    {
+                        using (SqlDataReader reader = command2.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                x++;
+                            }
+                        }
+                    }
+                    sname = new string[x];
+                    stype = new string[x];
+                    addr1= new string[x];
+                    lat = new string[x];
+                    longt = new string[x];
+                    iscctv = new string[x];
+                    cctvcount = new int[x];
+                    int i = 0;
+                    using (SqlCommand command2 = new SqlCommand(sql2, connection))
+                    {
+                        using (SqlDataReader reader = command2.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                //Console.WriteLine("{0}", reader.GetString(0));
+                                stype[i] = reader.GetString(0);
+                                sname[i] = reader.GetString(1);
+                                addr1[i] = reader.GetString(2);
+                                lat[i] = reader.GetString(3);
+                                longt[i] = reader.GetString(4);
+                                iscctv[i] = reader.GetString(5);
+                                cctvcount[i] = reader.GetInt32(6);
+                                i++;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
